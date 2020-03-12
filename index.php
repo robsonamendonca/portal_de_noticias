310ae1c4fb66bb1ae73eec83037ea5523cf7329e
wissae web <wissae@live.com>
2020-03-12 14:46:18 -0300

    creation of index / style.css and boot.css

--- index.php
+++ index.php
@@ -1,1 +1,59 @@
-<?php
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/boot.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Umuarama News - O portal de notícias em vídeos de umuarama</title>
+</head>
+<body>
+
+<header class="header_top rg_red fixed-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2">
+                <div>
+                    <a href="#"><img src="imagens/logo.png" alt=""></a>
+                </div>
+            </div>
+
+            <div class="col-md-10">
+                <div class="d-flex justify-content-end">
+                    <nav class="nav">
+                        <ul><li><a href="#">HOME</a></li></ul>
+                        <ul><li><a href="#">noticias</a></li></ul>
+                        <ul><li><a href="#">política</a></li></ul>
+                        <ul><li><a href="#">saúde</a></li></ul>
+                        <ul><li><a href="#">eventos</a></li></ul>
+                        <ul><li><a href="#">maranhão</a></li></ul>
+                        <ul><li><a href="#">nacional</a></li></ul>
+                        <ul><li><a href="#">mundo</a></li></ul>
+                        <ul><li><a href="#">contato</a></li></ul>
+
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+</body>
+</html>
